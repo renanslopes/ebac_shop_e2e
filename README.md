1be2952467bc53de2fd13236a5f4ccce87cdfe84
Renan Lopes <renan.lop@hotmail.com>
2024-11-28 16:03:19 -0300

    Ajuste do README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ebac_shop_e2e
+# Ebac Shop - Testes E2E
+Projeto de testes de interface E2E, feito em cooperação com meu amigo [Isac Oliveira](https://github.com/ByIsac).
+Os testes foram escritos e gerenciados na ferramenta [Qase](https://app.qase.io/), e os automações feitas utlizando VsCode + Cypress.
+Esse README será ainda incrementado e atualizado com as informações desse pequeno projeto colaborativo.
+
--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,22 @@
-/// <reference types="cypress" />
+// <reference types="cypress" />
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
+    /*  Como cliente 
+        Quero acessar a Loja EBAC 
+        Para fazer um pedido de 4 produtos 
+        Fazendo a escolha dos produtos
+        Adicionando ao carrinho
+        Preenchendo todas opções no checkout
+        E validando minha compra ao final */
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
+    beforeEach(() => {
+        cy.visit('/')
+    });
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
+
+    });
 
 
 })
